79d76dbf5582545dd260cb2a388d56ceae0228bf
mpan0025 <111552303+mpan0025@users.noreply.github.com>
2022-09-25 00:52:54 +1000

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,3 @@
+var vg_1 = "world.population.json"; vegaEmbed("#map1", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
